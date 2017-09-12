7ed55b3caed4c50df7dd3d65f643fcedc86ee196
ivantiu <ivankentiu@gmail.com>
2017-09-12 17:39:16 +0800

    Custom Navigation Bar Manually

--- TwitterLBTA/HomeDataSourceController.swift
+++ TwitterLBTA/HomeDataSourceController.swift
@@ -12,9 +12,50 @@ class HomeDataSourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigationBarItems()
+        
         // construct the HomeDataSource
         let homeDatasource = HomeDataSource()
         self.datasource = homeDatasource
+    }
+    
+    // custom Navigation Bar
+    private func setupNavigationBarItems() {
+        
+        // Title Image (frame to make it smaller)
+        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
+        titleImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        titleImageView.contentMode = .scaleAspectFit
+        
+        navigationItem.titleView = titleImageView
+        
+        // Follow Button on the left
+        let followButton = UIButton(type: .system)
+        // color of not the same so renderingmode!
+        followButton.setImage(#imageLiteral(resourceName: "follow").withRenderingMode(.alwaysOriginal), for: .normal)
+        // requires a frame to show up
+        followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
+        
+        
+        // 2 Buttons on the right
+        let searchButton = UIButton(type: .system)
+        searchButton.setImage(#imageLiteral(resourceName: "search").withRenderingMode(.alwaysOriginal), for: .normal)
+        searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        
+        let composeButton = UIButton(type: .system)
+        composeButton.setImage(#imageLiteral(resourceName: "compose").withRenderingMode(.alwaysOriginal), for: .normal)
+        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        // plural version of rightBarButtonItem [Array] left to right
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: composeButton), UIBarButtonItem(customView: searchButton)]
+        
+        // background color of the navigation bar (navigation has default translucency value(can be disabled))
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+        
     }
     
     // Line Spacing For Each Cell
